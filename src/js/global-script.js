0f4edced5ed0ba83a75998504656f093fb804b70
Nikolay Gromov <nicothin@gmail.com>
2017-05-29 11:12:27 +0300

    Модальные окна, календУрь

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -11,4 +11,13 @@ $( document ).ready(function() {
   // включение полифила для object-fit: cover; в днищебраузерах
   objectFitImages();
 
+  // включение датапикера в модальном окне и блокировка нескольких дат
+  var blockedDates = ["2017-05-12","2017-05-13","2017-05-14","2017-05-15","2017-05-16"]
+  $('#modal-datepicker').datepicker({
+    beforeShowDay: function(date){
+      var string = jQuery.datepicker.formatDate('yy-mm-dd', date);
+      return [ blockedDates.indexOf(string) == -1 ]
+    }
+  });
+
 });
