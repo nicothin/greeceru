6e8c85d81cc296442d8adddfb49a4f66773c3a57
Nikolay Gromov <nicothin@gmail.com>
2017-05-30 13:17:23 +0300

    Добавил карусели

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -20,4 +20,61 @@ $( document ).ready(function() {
     }
   });
 
+  // узнаем ширину скролла
+  var div = document.createElement('div');
+  div.style.overflowY = 'scroll';
+  div.style.width = '50px';
+  div.style.height = '50px';
+  div.style.visibility = 'hidden';
+  document.body.appendChild(div);
+  var scrollWidth = div.offsetWidth - div.clientWidth;
+  document.body.removeChild(div);
+
+  // карусели в блоках Top Rated
+  var $topRatedCarousel = $('.top-rated__list--carousel');
+  var topRatedCarouselSettings = {
+    margin: 15,
+    autoWidth: true,
+    loop: true,
+  };
+  function topRatedCarouselInit(){
+    var containerWidth = $(window).width() + scrollWidth;
+    // console.log('$(document).width(): '+containerWidth);
+    if(containerWidth <= 969) {
+      $topRatedCarousel.owlCarousel( topRatedCarouselSettings );
+    } else {
+      $topRatedCarousel.trigger('destroy.owl.carousel').removeClass('owl-carousel owl-loaded');
+      $topRatedCarousel.find('.owl-stage-outer').children().unwrap();
+    }
+  }
+  var id;
+  $(window).resize( function() {
+    clearTimeout(id);
+    id = setTimeout(topRatedCarouselInit, 200);
+  });
+  topRatedCarouselInit();
+
+  // карусель отзывов
+  $('#feedback-slider').owlCarousel({
+    responsive : {
+      0 : {
+        autoWidth: true,
+        loop: true,
+      },
+      1170 : {
+        autoWidth: false,
+        items: 3,
+        loop: true,
+      },
+    }
+  });
+  $('#feedback-slider-next').on('click', function(e){
+    e.preventDefault();
+    $('#feedback-slider').trigger('next.owl.carousel');
+  });
+  $('#feedback-slider-prev').on('click', function(e){
+    e.preventDefault();
+    $('#feedback-slider').trigger('prev.owl.carousel');
+  });
+
 });
