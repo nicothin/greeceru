ccb5f2ce216319a213c01fc765739cae45e916da
Nikolay Gromov <nicothin@gmail.com>
2017-06-24 21:37:42 +0300

    LP: шаг 3 (формы)

--- src/blocks/field-file/field-file.js
+++ src/blocks/field-file/field-file.js
@@ -4,29 +4,29 @@
   Available for use under the MIT License
 */
 
-;( function ()
-{
-  var inputs = document.querySelectorAll( '.field-file__input' );
-  Array.prototype.forEach.call( inputs, function( input )
-  {
-    var label  = input.closest('.field-file').querySelector( '.field-file__name-text' ),
-        labelVal = label.innerHTML;
+// ;( function ()
+// {
+//   var inputs = document.querySelectorAll( '.field-file__input' );
+//   Array.prototype.forEach.call( inputs, function( input )
+//   {
+//     var label  = input.closest('.field-file').querySelector( '.field-file__name-text' ),
+//         labelVal = label.innerHTML;
 
-    input.addEventListener( 'change', function( e ) {
-      var fileName = '';
-      if( this.files && this.files.length > 1 ) {
-        fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
-      }
-      else {
-        fileName = e.target.value.split( '\\' ).pop();
-      }
+//     input.addEventListener( 'change', function( e ) {
+//       var fileName = '';
+//       if( this.files && this.files.length > 1 ) {
+//         fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
+//       }
+//       else {
+//         fileName = e.target.value.split( '\\' ).pop();
+//       }
 
-      if( fileName ) {
-        label.innerHTML = fileName;
-      }
-      else {
-        label.innerHTML = labelVal;
-      }
-    });
-  });
-}());
+//       if( fileName ) {
+//         label.innerHTML = fileName;
+//       }
+//       else {
+//         label.innerHTML = labelVal;
+//       }
+//     });
+//   });
+// }());
