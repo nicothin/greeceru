e71ab6e435c335c4a9d43f09f5f206d1798c04a1
Nikolay Gromov <nicothin@gmail.com>
2019-06-01 23:57:48 +0300

    Добавление ректинга: мобилка без тайпохеда снизу

--- src/blocks/rating-add/rating-add.js
+++ src/blocks/rating-add/rating-add.js
@@ -1,4 +1,20 @@
-// document.addEventListener('DOMContentLoaded', function(){});
-// (function(){
-// код
-// }());
+(function(){
+
+  $('.rating-add__btn-next').on('click', ratingNextStep);
+  $('.rating-add__btn-back, .rating-add__like-back').on('click', ratingBackStep);
+
+  function ratingNextStep() {
+    var currentStep = $('.rating-add__step--active');
+    var nextStep = $(currentStep).next();
+    $(currentStep).removeClass('rating-add__step--active');
+    $(nextStep).addClass('rating-add__step--active');
+  }
+
+  function ratingBackStep() {
+    var currentStep = $('.rating-add__step--active');
+    var prevStep = $(currentStep).prev();
+    $(currentStep).removeClass('rating-add__step--active');
+    $(prevStep).addClass('rating-add__step--active');
+  }
+
+}());
